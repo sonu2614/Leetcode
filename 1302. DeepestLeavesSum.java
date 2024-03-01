03b23f5c6b281f085bb0f974ec8f9cc308338df3
Sonu <chaudharysonu2614@gmail.com>
2024-03-01 07:49:21 +0530

    Medium

--- 1302. DeepestLeavesSum.java
+++ 1302. DeepestLeavesSum.java
@@ -33,6 +33,71 @@
  * }
  * }
  */
+
+// import java.util.LinkedList;
+// import java.util.Queue;
+
+// class TreeNode {
+// int val;
+// TreeNode left;
+// TreeNode right;
+
+// TreeNode() {
+// }
+
+// TreeNode(int val) {
+// this.val = val;
+// }
+
+// TreeNode(int val, TreeNode left, TreeNode right) {
+// this.val = val;
+// this.left = left;
+// this.right = right;
+// }
+// }
+
+// class Solution {
+// public int deepestLeavesSum(TreeNode root) {
+// if (root == null) {
+// return 0;
+// }
+// int sum = 0;
+// Queue<TreeNode> q = new LinkedList<>();
+// q.add(root);
+// while (!q.isEmpty()) {
+// int size = q.size();
+// sum = 0;
+// for (int i = 0; i < size; i++) {
+// TreeNode curr = q.poll();
+// sum += curr.val;
+// if (curr.left != null) {
+// q.add(curr.left);
+// }
+// if (curr.right != null) {
+// q.add(curr.right);
+// }
+// }
+// }
+// return sum;
+// }
+// }
+
+// public class DeepestLeavesSum {
+// public static void main(String[] args) {
+// TreeNode root = new TreeNode(1);
+// root.left = new TreeNode(2);
+// root.right = new TreeNode(3);
+// root.left.left = new TreeNode(4);
+// root.left.right = new TreeNode(5);
+// root.right.right = new TreeNode(6);
+// root.left.left.left = new TreeNode(7);
+// root.right.right.right = new TreeNode(8);
+
+// Solution solution = new Solution();
+// int deepestLeavesSum = solution.deepestLeavesSum(root);
+// System.out.println("Sum of deepest leaves: " + deepestLeavesSum);
+// }
+// }
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
         if (root == null) {
